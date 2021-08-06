ca0e6a3e078d4d86f1c26de9caa7cea429dabc5d
Nliasso <nliasso@hotmail.com>
2021-08-06 13:23:25 -0300

    Tarea realizada hasta numeros primos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -69,16 +69,16 @@ function colors(color) {
   switch (color) {
     case 'blue':
       return 'This is blue';
-    break;
+    
     case 'red':
       return 'This is red';
-    break;
+    
     case 'green':
       return 'This is green';
-    break;
+    
     case 'orange':
       return 'This is orange';
-    break;
+    
     default:
       return 'Color not found';
   }
@@ -147,8 +147,23 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
-}
+  if (num1 <= -1 || num2 <= -1 || num3 <= -1 ) {
+    return "Hay negativos";
+
+  }else if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+
+  }else if (num1 > num2 && num1 > num3 && num1 > 0 ) {
+    return "Número 1 es mayor y positivo";
+    
+  }else if (num3 > num1 && num3 > num2) {
+    return (num3 + 1);
+
+  } else { 
+    return false
+  }
+} 
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -156,6 +171,7 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
 }
 
 function esVerdadero(valor){
